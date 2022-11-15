b697d9c451b1fe1d3aad7e590b12852ac06a5062
Niklas Petermeier <niklasp2209@outlook.de>
2022-11-15 18:11:17 +0100

    Initial commit

---
+++ src/main/java/de/niklas/bedwars/Bedwars.java
@@ -0,0 +1,16 @@
+package de.niklas.bedwars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Bedwars extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+        
+    }
+}
